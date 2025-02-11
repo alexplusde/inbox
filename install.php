6db838b53236a933dd12b4fdeabb05ef74cb6bdd
Alexander Walther <mail@alexplus.de>
2025-02-11 18:26:59 +0100

    Tabelle mit rex_sql_table vorbereiten

--- install.php
+++ install.php
@@ -5,8 +5,24 @@ namespace Alexplusde\Inbox;
 use rex;
 use rex_addon;
 use rex_file;
+use rex_sql_column;
+use rex_sql_table;
 use rex_yform_manager_table;
 use rex_yform_manager_table_api;
+
+rex_sql_table::get(rex::getTable('inbox'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('name', 'varchar(191)', false, ''))
+    ->ensureColumn(new rex_sql_column('email', 'varchar(191)', false, ''))
+    ->ensureColumn(new rex_sql_column('phone', 'varchar(191)', false, ''))
+    ->ensureColumn(new rex_sql_column('message', 'text'))
+    ->ensureColumn(new rex_sql_column('privacy_policy', 'datetime'))
+    ->ensureColumn(new rex_sql_column('preffered_channel', 'varchar(191)'))
+    ->ensureColumn(new rex_sql_column('datestamp', 'datetime'))
+    ->ensureColumn(new rex_sql_column('deletedate', 'datetime'))
+    ->ensureColumn(new rex_sql_column('ip', 'varchar(191)'))
+    ->ensureColumn(new rex_sql_column('form_url', 'text'))
+    ->ensure();
 
 /* Tablesets aktualisieren */
 $addon = rex_addon::get('inbox');
